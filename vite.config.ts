34966e7f4ab27163e61f73e1e9810e59f0e4b4f9
Amit Rajput <amitrajput123.ar@gmail.com>
2024-03-15 13:19:12 +0530

    new build conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  //  https://vitejs.dev/guide/static-deploy#github-pages
+  base: "/pretty-print-ai-rjson/"
 })
